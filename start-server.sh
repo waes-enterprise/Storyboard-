5fa8e2c499e55df605173b05e7e079b7ee9d0113
Z User <z@container>
2026-05-02 13:08:19 +0000

    Clean repo: remove internal tooling, DB, uploads, and skills

--- start-server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /home/z/my-project
-while true; do
-  npx next dev -p 3000 >> /home/z/my-project/server.log 2>&1
-  echo "$(date): Next.js died, restarting in 2s..." >> /home/z/my-project/server.log
-  sleep 2
-done
